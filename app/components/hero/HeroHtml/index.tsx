88f02188d7d61083815ec8ebf6ebc27ee80619cf
Bryan Maina <c.est@live.be>
2023-08-21 13:04:16 -0400

    - added top nav bar mock

--- app/components/hero/HeroHtml/index.tsx
+++ app/components/hero/HeroHtml/index.tsx
@@ -1,10 +1,14 @@
 import {useRef} from "react";
+import styles from "./hero.module.scss";
 
 export default function HeroHtml() {
     const ref = useRef<HTMLDivElement>(null!)
 
 
-    return <div ref={ref} className={"h-screen w-screen"}>
-        <h1>Hero Section</h1>
+    return <div ref={ref} className={styles.body}>
+        <div className={styles.element}><h1>Home</h1></div>
+        <div className={styles.element}><h1>About</h1></div>
+        <div className={`${styles.element} ${styles.active}`}><h1>Work</h1></div>
+        <div className={styles.element}><h1>Contact</h1></div>
     </div>
 }
